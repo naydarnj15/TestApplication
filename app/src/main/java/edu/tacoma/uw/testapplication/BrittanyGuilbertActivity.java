76360e37e64a3fc0444df357b2dbf9871bb06b1f
BBGuilbert <brittanybguilbert@gmail.com>
2020-04-12 16:10:03 -0700

    Adds an activity
    does absolutely nothing but add a single activity

---
+++ app/src/main/java/edu/tacoma/uw/testapplication/BrittanyGuilbertActivity.java
@@ -0,0 +1,14 @@
+package edu.tacoma.uw.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrittanyGuilbertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brittany_guilbert);
+    }
+}
