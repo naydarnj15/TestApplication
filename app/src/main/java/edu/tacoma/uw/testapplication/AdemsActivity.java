2e65201f8503634ea347e7f4fe4f73b27e952a4b
Saladin Adam <adems@uw.edu>
2020-04-13 01:12:59 -0700

    adems version

---
+++ app/src/main/java/edu/tacoma/uw/testapplication/AdemsActivity.java
@@ -0,0 +1,14 @@
+package edu.tacoma.uw.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adems);
+    }
+}
